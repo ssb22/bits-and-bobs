196fa776f486d1248fb016281d654f5a0c2297cf
Silas S. Brown <ssb22@cam.ac.uk>
2019-06-25 08:20:25 +0100

    repo setup don't forget email set

--- repo-setup.sh
+++ repo-setup.sh
@@ -11,10 +11,12 @@ while ! test "a$1" == a; do
   if ! test -d "$N"; then shift; continue; fi # so you can run with * in a dir with files as well
   # Otherwise put the settings in on an already-cloned repo:
   cd $N || exit 1
-  git remote set-url origin --push --delete . || exit 1
+  git remote set-url origin --push --delete . || true
   git remote set-url origin --push git@github.com:ssb22/$N.git || exit 1
   git remote set-url origin --push --add git@gitlab.com:ssb22/$N.git || exit 1
   git remote set-url origin --push --add git@bitbucket.org:ssb22/$(echo $N|tr A-Z a-z).git || exit 1
+  git config user.name "Silas S. Brown"
+  git config user.email ssb22@cam.ac.uk
   cd ..
   echo "Set up $N"
   shift
