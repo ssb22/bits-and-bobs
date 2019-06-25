eea8cfe4a7fa60cb4b4a57457c110d85034a702b
Silas S. Brown <ssb22@hermes.cam.ac.uk>
2019-06-25 13:34:52 +0100

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git pull
+git pull --no-edit
 wget -N http://people.ds.cam.ac.uk/ssb22/setup/ssh-android.sh http://people.ds.cam.ac.uk/ssb22/mwrhome/offline-updater.py http://people.ds.cam.ac.uk/ssb22/notes/flatplan.py http://people.ds.cam.ac.uk/ssb22/notes/notesorg_lua.txt http://people.ds.cam.ac.uk/ssb22/notes/notesorg_macro.txt http://people.ds.cam.ac.uk/ssb22/papers/notesorg.py http://people.ds.cam.ac.uk/ssb22/s60/cdrdao.py http://people.ds.cam.ac.uk/ssb22/setup/riscos-time.py http://people.ds.cam.ac.uk/ssb22/setup/pounce.py http://people.ds.cam.ac.uk/ssb22/setup/record-skype.py http://people.ds.cam.ac.uk/ssb22/setup/msn-rename.py http://people.ds.cam.ac.uk/ssb22/setup/narrow-reader.sh http://people.ds.cam.ac.uk/ssb22/setup/macvoices.txt http://people.ds.cam.ac.uk/ssb22/setup/openid-cli.py http://people.ds.cam.ac.uk/ssb22/adjuster/xml2csv.py http://people.ds.cam.ac.uk/ssb22/adjuster/xml2band.py http://people.ds.cam.ac.uk/ssb22/adjuster/xml2dx.py http://people.ds.cam.ac.uk/ssb22/adjuster/gitify.sh
 git commit -am "Update $(echo $(git diff|grep '^--- a/'|sed -e 's,^--- a/,,')|sed -e 's/ /, /g')" && git push
