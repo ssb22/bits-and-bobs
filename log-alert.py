15883abc57b59e902960a35d509534d7d2978a97
Silas S. Brown <ssb22@cam.ac.uk>
2019-12-22 08:25:28 +0000

    Update log-alert.py

--- log-alert.py
+++ log-alert.py
@@ -20,23 +20,27 @@ log_file = "/var/log/apache2/user/ssb22/access.log"
 
 min_bytes_to_report = 5000000 # less than this per IP = IP won't be reported no matter how many requests
 min_requests_to_report = 10 # less than this per IP = IP won't be reported no matter how many bytes
-min_sameFile_to_report = 2 # must contain at least N attempts on the same file to be reported
+min_sameFile_to_report = 7 # must contain at least N attempts on the same file to be reported
+min_sameFile_size = 1000000 # and in total across all attempts (ignore browsers failing to cache small css files)
 max_age = 25*3600 # ignore log lines older than this
 
 import sys,time
 
-ipBytes,ipReqs,ipURLCounts,ipLog = {},{},{},{}
+ipBytes,ipReqs,ipURLCounts,ipURLSizes,ipLog = {},{},{},{},{}
 for l in open(log_file):
     ip,_,_,date,tz,get,url,method,code,size,rest = l.split(None,10)
     if time.time()-time.mktime(time.strptime(date[1:],"%d/%b/%Y:%H:%M:%S")) > max_age: continue
     ipBytes[ip]=ipBytes.get(ip,0)+int(size)
     ipReqs[ip]=ipReqs.get(ip,0)+1
-    if not ipURLCounts.setdefault(ip,{}).setdefault(url,0) or not code=="206": ipURLCounts[ip][url] += 1
+    if not ipURLCounts.setdefault(ip,{}).setdefault(url,0) or not code=="206":
+        ipURLCounts[ip][url] += 1
+        ipURLSizes.setdefault(ip,{}).setdefault(url,[]).append(int(size))
     ipLog.setdefault(ip,[]).append(l.strip())
 o = []
 for ip in ipBytes.keys():
     if ipBytes[ip] < min_bytes_to_report: continue
     if ipReqs[ip] < min_requests_to_report: continue
     if all(v<min_sameFile_to_report for v in ipURLCounts[ip].values()): continue
+    if all((sum(v)<min_sameFile_size or ipURLCounts[ip][k]<min_sameFile_to_report) for k,v in ipURLSizes[ip].items()): continue
     o.append("\n".join(ipLog[ip]))
-sys.stdout.write("\n\n".join(o)+"\n")
+if o: sys.stdout.write("\n\n".join(o)+"\n")
