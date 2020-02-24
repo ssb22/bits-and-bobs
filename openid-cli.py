73430a5c3f684763bce637bee97abd9fc698f3e6
Silas S. Brown <ssb22@cam.ac.uk>
2020-02-24 00:10:16 +0000

    Update openid-cli.py

--- openid-cli.py
+++ openid-cli.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+# (should work in eithepr Python 2 or Python 3)
 
-print "OpenID Command-line Authoriser v1.1, Silas S. Brown 2017 (public domain, no warranty)\n"
-
-from openid_config import local_addr, public_endpoint_url, profile
+print ("OpenID Command-line Authoriser v1.3, Silas S. Brown 2017, 2020")
+print ("(public domain, no warranty)\n")
 
 import sys
 try: import openid
 except ImportError: sys.stderr.write("Library not found\nTry: sudo apt-get install python-openid\n"),sys.exit(1)
-print "Make sure your home page <head> section has:"
-print '<link rel="openid.server" href="'+public_endpoint_url+'" />\n'
-print "Loading...", ; sys.stdout.flush()
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from urlparse import urlparse
+try: from openid_config import local_addr, public_endpoint_url, profile
+except: sys.stderr.write("openid_config.py not found, please make one\n"),sys.exit(1)
+print ("Make sure your home page <head> section has:")
+print ('<link rel="openid.server" href="'+public_endpoint_url+'" />\n')
+sys.stdout.write("Loading... ") ; sys.stdout.flush()
+try: from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler # Python 2
+except: from http.server import HTTPServer, BaseHTTPRequestHandler # Python 3
+try: from urlparse import urlparse # Python 2
+except: from urllib.parse import urlparse # Python 3
 from cgi import parse_qsl
 from openid.server import server
 from openid.store.memstore import MemoryStore
 from openid.extensions import sreg
 OpenID = server.Server(MemoryStore(), public_endpoint_url)
+try: raw_input
+except: raw_input = input # Python 3
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         query = {}
@@ -33,15 +39,15 @@ class Handler(BaseHTTPRequestHandler):
     def log_message(*args): pass # (not needed if nginx is doing it)
     def handleQ(self, query):
         try: req = OpenID.decodeRequest(query)
-        except server.ProtocolError, e: return self.encodeAndSend(e)
+        except server.ProtocolError as e: return self.encodeAndSend(e)
         if req==None: self.send_response(200),self.end_headers()
         elif req.mode in ["checkid_immediate", "checkid_setup"]: self.checkID(req)
         else: self.encodeAndSend(OpenID.handleRequest(req))
     def checkID(self, req):
-        print "\nGot",req.mode,"request for",req.identity,"via",self.client_address[0] # (must include client_address to protect against somebody else staging a login to the same site at the same time)
+        print ("\nGot "+req.mode+" request for "+req.identity+" via "+self.client_address[0]) # (must include client_address to protect against somebody else staging a login to the same site at the same time)
         yn = ''
         while not yn.lower() in ['y','n']:
-            print "Send this ID to "+req.trust_root+"? (y/n): ",
+            print ("Send this ID to "+req.trust_root+"? (y/n): "),
             yn = raw_input()
         if yn.lower()=='y':
             r = req.answer(True,identity=req.identity)
@@ -56,5 +62,5 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
         if r.body: self.wfile.write(r.body)
 httpserver = HTTPServer(local_addr, Handler)
-print "ready for requests (Ctrl-C to quit)"
+print ("ready for requests (Ctrl-C to quit)")
 httpserver.serve_forever()
