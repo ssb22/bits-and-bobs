1f13b798cdd5d12b2857cf4345d1dcc72ed10783
Silas S. Brown <ssb22@cam.ac.uk>
2020-05-25 10:22:04 +0100

    gitify: staged push, for archiving old projects over unreliable connections

--- gitify.sh
+++ gitify.sh
@@ -5,7 +5,7 @@
 
 # The reverse of Rodrigo Silva's "git-restore-mtime" script.
 
-# Silas S. Brown 2019 - public domain - no warranty.
+# v1.3, Silas S. Brown 2019-20, public domain, no warranty
 
 # Might be useful for "git"ifying historical code, as at
 # least the file's modification time should put an upper
@@ -13,8 +13,14 @@
 
 # Top-level dotfiles will NOT be added (so .git itself won't be)
 
-# Use --add to add files to an existing repo,
+# Use --add to add files to an existing repository,
 # or --rewrite to rewrite all.
+
+# Use --addpush to behave like --add but do a "git push -u origin master"
+# after every commit (might be useful on connections that
+# cannot do a large push at the end).  This can be used
+# only if you've already done "git init" and any necessary
+# "git remote add origin" commands.
 
 # --day (optional parameter) limits commits to one per day,
 # with the last modification time used in any given day.
@@ -36,12 +42,15 @@ if test "a$1" == "a--rewrite"; then # (must be run at top-level of the repo)
     mv /tmp/old-git-config .git/config
 elif test "a$1" == "a--add"; then # (this one may also be run in a subdirectory)
     if ! git rev-parse --git-dir >/dev/null 2>/dev/null; then git init; elif git diff | grep . >/dev/null; then git diff; echo "ERROR: Commit these diffs first"; exit 1; fi
+elif test "a$1" == "a--addpush"; then # (this one may also be run in a subdirectory)
+    if ! git rev-parse --git-dir >/dev/null 2>/dev/null; then echo "ERROR: --addpush requires an already-existing repository"; exit 1; elif git diff | grep . >/dev/null; then git diff; echo "ERROR: Commit these diffs first"; exit 1; fi
 else
     echo "Run with --rewrite to delete existing history and rewrite from scratch"
     echo "(in case you've already created a git repo w/out this script)"
     echo "Run with --add to just add new files to the repo"
+    echo "(or --addpush to do this while pushing every commit)"
     echo
-    echo "You MUST specify either --rewrite or --add."
+    echo "You MUST specify either --rewrite or --add or --addpush."
     echo "You may also specify --day to limit commits to one per day (and to ensure commits are in date order)."
     exit 1
 fi
@@ -49,7 +58,10 @@ fi
 if test "a$2" == "a--day"; then export Day=1; fi
 
 if test "a$Day" == a1; then
-find * -type f -not -name '*~' -not -name .DS_Store -exec python -c 'import os;print(str(os.stat("{}").st_mtime)+" {}")' ';' 2>/dev/null|python -c $'import sys,time,os,pipes\ndef cond(a,b,c):\n if a: return b\n return c\ndf,dt={},{}\nfor d,t,f in sorted([(int(float(l.split()[0])/(24*3600)),float(l.split()[0]),l.split(None,1)[1].rstrip()) for l in sys.stdin]):\n if not d in df: df[d],dt[d]=set(),0\n df[d].add(f);dt[d]=max(dt[d],t)\nfor d in sorted(df.keys()):\n os.environ["GIT_COMMITTER_DATE"]=time.asctime(time.localtime(dt[d]))\n os.system("git add %s && git commit --date=\\"$GIT_COMMITTER_DATE\\" -am \\"add %s\\"" % (" ".join(pipes.quote(x) for x in sorted(df[d])),cond(len(df[d])>5,"%d files" % len(df[d]),", ".join(sorted(df[d])))))'
+  if test "a$1" == "a--addpush"; then export ExtraCmd="git push -u origin master"; else unset ExtraCmd; fi
+  find * -type f -not -name '*~' -not -name .DS_Store -exec python -c 'import os;print(str(os.stat("{}").st_mtime)+" {}")' ';' 2>/dev/null|python -c $'import sys,time,os,pipes\ndef cond(a,b,c):\n if a: return b\n return c\ndf,dt={},{}\nfor d,t,f in sorted([(int(float(l.split()[0])/(24*3600)),float(l.split()[0]),l.split(None,1)[1].rstrip()) for l in sys.stdin]):\n if not d in df: df[d],dt[d]=set(),0\n df[d].add(f);dt[d]=max(dt[d],t)\nfor d in sorted(df.keys()):\n os.environ["GIT_COMMITTER_DATE"]=time.asctime(time.localtime(dt[d]))\n r=os.system("git add %s && git commit --date=\\"$GIT_COMMITTER_DATE\\" -am \\"add %s\\"" % (" ".join(pipes.quote(x) for x in sorted(df[d])),cond(len(df[d])>5,"%d files" % len(df[d]),", ".join(sorted(df[d])))))\n if "ExtraCmd" in os.environ and not r and os.system(os.environ["ExtraCmd"]): raise Exception("git push failed or interrupted")'
+  # (if files already added, 'git commit' will fail and 'git push' won't happen; if one 'git push' fails, next one will be larger, so we check for fail so interrupt / resume is easier)
 else
-find * -type f -not -name '*~' -not -name .DS_Store -exec git add '{}' ';' -exec bash -c 'export D="$(date -r "{}" 2>/dev/null || stat -f %Sm "{}")";GIT_COMMITTER_DATE="$D" git commit --date="$D" -am "add {}"' ';'
+  if test "a$1" == "a--addpush"; then export Extras="-exec git push -u origin master ;"; else unset Extras; fi
+  find * -type f -not -name '*~' -not -name .DS_Store -exec git add '{}' ';' -exec bash -c 'export D="$(date -r "{}" 2>/dev/null || stat -f %Sm "{}")";GIT_COMMITTER_DATE="$D" git commit --date="$D" -am "add {}"' ';' $Extras
 fi && if test "a$1" == "a--rewrite"; then git push -f; fi
