61f24c4f88ae0c3e82a15d1583e4c473bc3513e6
Silas S. Brown <ssb22@cam.ac.uk>
2020-08-29 09:38:43 +0100

    repo-setup more options

--- repo-setup.sh
+++ repo-setup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 # 
-# Set up push access to GitHub, GitLab and BitBucket
-# so git-sync.sh scripts will work from ssb22's account.
-# (Assumes the repositories have already been created / imported.)
+# Set up push access to GitHub, GitLab, GitLab-cam, Bitbucket, Gitee
+# (for repositories already created / imported across these platforms)
 # 
-if test "a$1" == a; then echo "Missing repo name"; exit 1; fi
-while ! test "a$1" == a; do
+if [ "$1" == "" ]; then
+    echo "Syntax: repo-setup [--shallow] all | repo names"
+    exit 1
+elif [ "$1" == --shallow ]; then
+    export RepoSetupShallowClone="--depth=1"
+    shift
+elif ! [ "$RepoSetupShallowClone" == "--depth=1" ]; then
+    unset RepoSetupShallowClone
+fi
+if [ "$1" == all ]; then
+    exec "$0" CedPane PrimerPooler adjuster css-generator web-imap-etc config lexconvert indexer web-typography jianpu-ly mwr2ly midi-beeper scan-reflow bits-and-bobs router-scripts wm6-utils s60-utils gradint yali-voice yali-lower cameron-voice 4dml old-web-access-gateway clara-empricost
+fi
+while ! [ "$1" == "" ]; do
   export N=$(echo "$1"|tr -d /) # in case of auto-complete
-  if ! test -e "$N"; then git clone git@github.com:ssb22/$N.git || exit 1; fi
-  if ! test -d "$N"; then shift; continue; fi # so you can run with * in a dir with files as well
+  if ! [ -e "$N" ]; then git clone $RepoSetupShallowClone git@github.com:ssb22/$N.git || exit 1; fi
+  if ! [ -d "$N" ]; then shift; continue; fi # so you can run with * in a dir with files as well
   # Otherwise put the settings in on an already-cloned repo:
   cd $N || exit 1
   git remote set-url origin --push --delete . 2>/dev/null >/dev/null || true
@@ -16,6 +26,7 @@ while ! test "a$1" == a; do
   git remote set-url origin --push --add git@gitlab.com:ssb22/$N.git || exit 1
   git remote set-url origin --push --add git@gitlab.developers.cam.ac.uk:ssb22/$N.git || exit 1 # (beta service provided to cam.ac.uk members)
   git remote set-url origin --push --add git@bitbucket.org:ssb22/$(echo $N|tr A-Z a-z).git || exit 1
+  git remote set-url origin --push --add git@gitee.com:ssb22/$(echo $N|sed -e s/^4/four/).git || exit 1
   git config user.name "Silas S. Brown"
   git config user.email ssb22@cam.ac.uk
   cd ..
