e5f158ed6fa6689c23e2efb97fc022e0b5c5aabe
Silas S. Brown <ssb22@cam.ac.uk>
2022-11-22 09:08:18 +0000

    update termux mosh script to work with newer packages

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,8 +1,9 @@
 # Script to paste into Termux after installing on a new device
+# Silas S. Brown - public domain - no warranty
 
 echo "deb https://packages-cf.termux.org/apt/termux-main stable main" > /data/data/com.termux/files/usr/etc/apt/sources.list && # replacing https://termux.astra.in.ua/apt/termux-main/
 pkg up &&
-pkg install openssh wget mosh expect perl exa joe lynx netcat-openbsd &&
+pkg install openssh wget mosh-git expect perl exa joe lynx netcat-openbsd &&
 # might want: clang git make nodejs python2 python tracepath man
 echo alias ls=exa >.bash_profile &&
 cd /data/data/com.termux/files/usr/bin &&
