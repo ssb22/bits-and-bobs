83ad49342da5f9bea1d9007110b63b4412f7735e
Silas S. Brown <ssb22@cam.ac.uk>
2022-12-22 10:11:30 +0000

    ds plans sftp-only (use rclone) 2023-01-20 (postponed from today)

--- termux-setup.sh
+++ termux-setup.sh
@@ -3,15 +3,10 @@
 
 echo "deb https://packages-cf.termux.org/apt/termux-main stable main" > /data/data/com.termux/files/usr/etc/apt/sources.list && # replacing https://termux.astra.in.ua/apt/termux-main/
 pkg up &&
-pkg install openssh wget mosh-git expect perl exa joe lynx netcat-openbsd &&
+pkg install openssh wget mosh-git exa joe lynx netcat-openbsd &&
 # might want: clang git make nodejs python2 python tracepath man
 echo alias ls=exa >.bash_profile &&
-cd /data/data/com.termux/files/usr/bin &&
-wget -O ds https://raw.githubusercontent.com/ssb22/bits-and-bobs/master/ds &&
-chmod +x ds &&
-for N in ds1 ds2 ds-ssh ds1-ssh ds2-ssh; do ln -s ds $N; done &&
-cd &&
 ssh-keygen &&
 cat .ssh/id_rsa.pub &&
-echo Copy the above into non-ds authorized_keys &&
-echo Add .ssh/config and .ssh/ds, and ssh manually to accept server keys first
+echo "Copy the above into servers' authorized_keys" &&
+echo Add .ssh/config
