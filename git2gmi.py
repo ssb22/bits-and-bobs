4c233302d9c33aae7668055e202bac349559ea5f
Silas S. Brown <ssb22@cam.ac.uk>
2023-01-26 08:06:25 +0000

    git2gmi better handling of short-history repositories

--- git2gmi.py
+++ git2gmi.py
@@ -20,8 +20,10 @@ max_earliest_date = None ; commitList = []
 for repository in json.loads(urlopen("https://api.github.com/users/"+user+"/repos").read()):
     is_fork = repository["fork"]
     repository = repository["name"]
+    count = 0
     for commit in json.loads(urlopen("https://api.github.com/repos/"+user+"/"+repository+"/commits?per_page=100").read()): # 100 is maximum allowed
-        if is_fork and not user in commit["commit"]["committer"]["email"]: continue # don't log other people's commits on our forks (assumes Git username is part of our email)
+        count += 1
+        if is_fork and not user in commit["commit"]["author"]["email"]: continue # don't log other people's commits on our forks (assumes Git username is part of our email)
         fullDate = commit["commit"]["committer"]["date"]
         date = fullDate[:fullDate.index('T')]
         message = commit["commit"]["message"]
@@ -29,6 +31,7 @@ for repository in json.loads(urlopen("https://api.github.com/users/"+user+"/repo
         if message.startswith("Merge") and "\n\n" in message: message=message[:message.index("\n\n")] # because commits themselves will be listed separately
         else: message = message.replace("\n\n","\n",1) # probably "This reverts commit N", don't need blank line before
         commitList.append((date,fullDate,"=> %s %s %s: %s" % (url,date,repository,message)))
-    if not max_earliest_date: max_earliest_date = date
-    elif date: max_earliest_date = max(date,max_earliest_date)
-print ("# Recent commits to "+user+"'s GitHub repositories\n"+"\n".join(x[-1] for x in reversed(sorted(commitList)) if x[0] >= max_earliest_date))
+    if count>=100 and not is_fork:
+        if not max_earliest_date: max_earliest_date = date
+        elif date: max_earliest_date = max(date,max_earliest_date)
+print ("# Recent commits to "+user+"'s GitHub repositories\n"+"\n".join(x[-1] for x in reversed(sorted(commitList)) if not max_earliest_date or x[0] >= max_earliest_date))
