9cf04c641d11d75787336f967d647cef3cbb3fa8
Silas S. Brown <ssb22@cam.ac.uk>
2023-02-16 08:41:49 +0000

    packages-cf no longer needed and is a no-op

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,7 +1,6 @@
 # Script to paste into Termux after installing on a new device
 # Silas S. Brown - public domain - no warranty
 
-echo "deb https://packages-cf.termux.org/apt/termux-main stable main" > /data/data/com.termux/files/usr/etc/apt/sources.list && # replacing https://termux.astra.in.ua/apt/termux-main/
 pkg up &&
 pkg install openssh wget mosh-git exa joe lynx netcat-openbsd &&
 # might want: clang git make nodejs python2 python tracepath man
