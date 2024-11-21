873b3f54dd0cd06857c652dda9d21e13715cd71a
Silas S. Brown <ssb22@cam.ac.uk>
2024-11-21 07:19:11 +0000

    brl2unicode might as well put here

---
+++ brl2unicode.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# translate ASCII Braille to Unicode Braille
+# Silas Brown - public domain
+import sys
+print(sys.stdin.read().translate(
+    dict((ord(x),y)
+         for x,y in zip(
+                 " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=", # (I was the one who put that on Wikipedia)
+                 list(range(0x2800,0x2840))))))
