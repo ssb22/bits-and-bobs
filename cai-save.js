2dedca2a5a526d680f79e9d6ffdd1bdd319646e1
Silas S. Brown <ssb22@cam.ac.uk>
2025-02-24 05:55:56 +0000

    comment

--- cai-save.js
+++ cai-save.js
@@ -1,6 +1,21 @@
-// c.ai LLM history saver - SSB 2024
+// c.ai LLM history saver - SSB 2024, public domain, no warranty
+
 // Ensure you've scrolled back enough to populate whole history.
-// Paste this line into DOM Inspector Console and copy the result.
+// (By default 50 messages are shown when you load the charater.
+// As of 2024, the URL, which can be shared with other logged-in
+// users, is the URL of the character, NOT of your specific chat
+// which cannot be shared by URL.  However, if you yourself load
+// a character, it will show your last 50 messages with them and
+// can show more if you scroll back.)
+
+// Paste this line into DOM Inspector Console + copy the result.
+
+// To backup further conversation with the same character, you
+// might not need to scroll all the way back to the beginning:
+// simply search for the known last thing said in the new markup
+// and if it's there you should then be able to copy starting
+// from after this point.
+
 // Tested in:
 // Chrome 131 on GNU/Linux (replace "\n" with newline after copy-paste)
 // Firefox 133 on GNU/Linux (right-click Copy Message: newlines intact)
