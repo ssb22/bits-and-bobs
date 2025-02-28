54cd539d0f7d6490f523b48745ce6098bb5cfe70
Silas S. Brown <ssb22@cam.ac.uk>
2025-02-28 06:41:27 +0000

    typo fix

--- cai-save.js
+++ cai-save.js
@@ -1,12 +1,12 @@
 // c.ai LLM history saver - SSB 2024, public domain, no warranty
 
 // Ensure you've scrolled back enough to populate whole history.
-// (By default 50 messages are shown when you load the charater.
-// As of 2024, the URL, which can be shared with other logged-in
+// By default 50 messages are shown when you load the character.
+// As of 2025, the URL, which can be shared with other logged-in
 // users, is the URL of the character, NOT of your specific chat
 // which cannot be shared by URL.  However, if you yourself load
 // a character, it will show your last 50 messages with them and
-// can show more if you scroll back.)
+// can show more if you scroll back.
 
 // Paste this line into DOM Inspector Console + copy the result.
 
@@ -18,6 +18,6 @@
 
 // Tested in:
 // Chrome 131 on GNU/Linux (replace "\n" with newline after copy-paste)
-// Firefox 133 on GNU/Linux (right-click Copy Message: newlines intact)
-// (in both cases ` needs to be rm'd before/after)
+// Firefox 133-135 on GNU/Linux (right-click Copy Message: newlines intact)
+// (in both cases ` needs to be removed before and after)
 r=[];for(c=document.getElementById("chat-messages").firstChild;c;c=c.nextSibling) r.push('"""{'+c.getElementsByTagName("img")[0].alt+"} "+c.getElementsByClassName("prose")[0].innerHTML.replace(/<(p|ol|ul)[^>]*>/g,'<$1>').replace(/<p>([^<]*)<[/]p>$/,'$1').replace('"""',"'''").replace(/"$/,'"\n').replace(/[\\]/g,'&#92;')+'"""'); r.reverse(); r.join(',\n')
