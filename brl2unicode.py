6f5c8b5deba2a32d69f1a900f418fe521d0ff359
Silas S. Brown <ssb22@cam.ac.uk>
2025-03-19 16:12:09 +0000

    some ASCII Braille generators use lower case

--- brl2unicode.py
+++ brl2unicode.py
@@ -2,7 +2,7 @@
 # translate ASCII Braille to Unicode Braille
 # Silas Brown - public domain
 import sys
-print(sys.stdin.read().translate(
+print(sys.stdin.read().upper().translate(
     dict((ord(x),y)
          for x,y in zip(
                  " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=", # (I was the one who put that on Wikipedia)
