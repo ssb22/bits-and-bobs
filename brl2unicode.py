1965724b5d1200a37819cba586fd9c5c750aae93
Silas S. Brown <ssb22@cam.ac.uk>
2025-07-24 08:20:08 +0100

    this string was removed from Wikipedia: https://en.wikipedia.org/w/index.php?title=Braille_ASCII&diff=prev&oldid=1297187723

--- brl2unicode.py
+++ brl2unicode.py
@@ -5,5 +5,5 @@ import sys
 print(sys.stdin.read().upper().translate(
     dict((ord(x),y)
          for x,y in zip(
-                 " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=", # (I was the one who put that on Wikipedia)
+                 " A1B'K2L@CIF/MSP\"E3H9O6R^DJG>NTQ,*5<-U8V.%[$+X!&;:4\\0Z7(_?W]#Y)=",
                  list(range(0x2800,0x2840))))))
