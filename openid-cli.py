9f4c0d2cce40adaff0616a93bb60bf3ed1bb9e99
Silas S. Brown <ssb22@cam.ac.uk>
2025-09-25 09:21:35 +0100

    openid Python 3 compatibility fixes

--- openid-cli.py
+++ openid-cli.py
@@ -8,12 +8,12 @@
 # and at https://gitlab.developers.cam.ac.uk/ssb22/bits-and-bobs
 # and in China: https://gitee.com/ssb22/bits-and-bobs
 
-print ("OpenID Command-line Authoriser v1.3, Silas S. Brown 2017, 2020")
+print ("OpenID Command-line Authoriser v1.4, Silas S. Brown 2017, 2020, 2025")
 print ("(public domain, no warranty)\n")
 
 import sys
 try: import openid
-except ImportError: sys.stderr.write("Library not found\nTry: sudo apt-get install python-openid\n"),sys.exit(1)
+except ImportError: sys.stderr.write("Library not found\nTry: sudo apt-get install python"+("3" if type(u"")==type("") else "")+"-openid\n"),sys.exit(1)
 try: from openid_config import local_addr, public_endpoint_url, profile
 except: sys.stderr.write("openid_config.py not found, please make one\n"),sys.exit(1)
 print ("Make sure your home page <head> section has:")
@@ -23,7 +23,8 @@ try: from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler # Python 2
 except: from http.server import HTTPServer, BaseHTTPRequestHandler # Python 3
 try: from urlparse import urlparse # Python 2
 except: from urllib.parse import urlparse # Python 3
-from cgi import parse_qsl
+try: from cgi import parse_qsl
+except: from urllib.parse import parse_qsl
 from openid.server import server
 from openid.store.memstore import MemoryStore
 from openid.extensions import sreg
@@ -45,6 +46,7 @@ class Handler(BaseHTTPRequestHandler):
         self.handleQ(query)
     def log_message(*args): pass # (not needed if nginx is doing it)
     def handleQ(self, query):
+        if type("")==type(u"") and type(list(query.keys())[0])==bytes: query=dict((k.decode('utf-8'),v.decode('utf-8')) for k,v in query.items())
         try: req = OpenID.decodeRequest(query)
         except server.ProtocolError as e: return self.encodeAndSend(e)
         if req==None: self.send_response(200),self.end_headers()
@@ -62,12 +64,15 @@ class Handler(BaseHTTPRequestHandler):
             r.addExtension(sreg.SRegResponse.extractResponse(sreg.SRegRequest.fromOpenIDRequest(req), profile))
         else: r = req.answer(False)
         self.encodeAndSend(r)
+        print ("Waiting for new request (Ctrl-C to quit)")
     def encodeAndSend(self, response):
         r = OpenID.encodeResponse(response)
         self.send_response(r.code)
-        for h,v in r.headers.iteritems(): self.send_header(h,v)
+        for h,v in r.headers.items(): self.send_header(h,v)
         self.end_headers()
-        if r.body: self.wfile.write(r.body)
+        if r.body:
+            if type(r.body)==type(u""): r.body=r.body.encode('utf-8')
+            self.wfile.write(r.body)
 httpserver = HTTPServer(local_addr, Handler)
 print ("ready for requests (Ctrl-C to quit)")
 httpserver.serve_forever()
