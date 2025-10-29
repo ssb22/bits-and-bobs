3b4e9dac4bebd7b4883c96faa1722723e1d774f0
Silas S. Brown <ssb22@cam.ac.uk>
2025-10-29 06:56:12 +0000

    LLM-save comments

--- chat-save-js/qwen.js
+++ chat-save-js/qwen.js
@@ -1,5 +1,10 @@
 // Javascript to paste into browser developer console
 // to help save a session of chat.qwen.ai
+
 // Silas S. Brown 2025 - public domain - no warranty
+
+// Ensure you've scrolled back enough to populate whole history.
+// Paste this line into DOM Inspector Console + copy the result.
+// Tested in Firefox 144 on GNU/Linux
 
 ((d)=>{var r=[],i;for(i=0;i<d.length;i++)r.push(((d[i].getAttribute("class").search("user-message")>-1)?"User: ":"Qwen: ")+d[i].innerText);return r.join('\n\n')})(document.querySelectorAll('.user-message-text-content, .response-message-body')) /* qwen-save */
