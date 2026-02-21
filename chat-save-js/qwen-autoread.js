fb981556e33563fbda704d7fcfb54815d5700b8f
Silas S. Brown <ssb22@cam.ac.uk>
2026-02-21 08:40:05 +0000

    Qwen auto-readaloud script

---
+++ chat-save-js/qwen-autoread.js
@@ -0,0 +1,27 @@
+// chat.qwen.ai auto-play the TTS in browser
+// Silas S. Brown 2026 - public domain
+
+// Paste this into the Console window
+// Tested in Firefox 147 on Ubuntu GNU/Linux 24.04
+
+function watchContainer(container) {
+    const doneAttr = 'data-auto-tts-processed';
+    if (container.hasAttribute(doneAttr)) return;
+    let triesLeft=50;
+    const check=setInterval(()=>{
+        if(container.children.length==6) {
+            clearInterval(check);
+            container.setAttribute(doneAttr, 't');
+            container.lastElementChild.click();
+            setTimeout(()=>{document.querySelector('.qwen-chat-package-comp-new-action-more-operation-text').click()},1000);
+        } else if(!triesLeft--)clearInterval(check)
+    }, 500)}
+
+new MutationObserver((mutations) => {
+    const ctrlIconsClass = 'qwen-chat-package-comp-new-action-control-icons';
+    mutations.forEach((mutation)=>{
+        mutation.addedNodes.forEach((node)=>{
+            if (node.nodeType===1) {
+                if(node.classList && node.classList.contains(ctrlIconsClass)) watchContainer(node);
+                node.querySelectorAll(`.${ctrlIconsClass}`).forEach(watchContainer);
+}})})}).observe(document.body,{childList:true,subtree:true});
