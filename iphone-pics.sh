ee2838ec3fbc9ab6b932a4b9f03eb6e5add51987
Silas S. Brown <ssb22@cam.ac.uk>
2026-04-04 08:15:34 +0100

    simplify

--- iphone-pics.sh
+++ iphone-pics.sh
@@ -9,9 +9,12 @@ User="$(stat -c '%U' .)" # in case we're running as root
 ThisBatch="$(pwd)/$(date +iphone-%Y-%m-%d)"
 mkdir "$ThisBatch"
 cd /mnt/DCIM
-for D in *; do cd "$D" && for F in *; do mv -v "$F"  "$ThisBatch/$D-$F";done; cd ..; done
-cd "$ThisBatch"
-# We assume we have bash 4+ if we have ifuse
-for F in *; do counter=1; D=$(date -r "$F" +%Y-%m-%d_%H-%M-%S 2>/dev/null); E="${F##*.}"; E="${E,,}"; N="${D}$(if ! [ $counter == 1 ] ; then printf '_%03d' $counter; fi).${E}"; while [ -e "$N" ]; do ((counter++)); N="${D}_$(printf '%03d' $counter).${E}"; done; mv -v "$F" "$N"; ((counter++)); done
-chown -R "$User:$User" .
+for Dir in *; do cd "$Dir"
+# Move off, rename by timestamp, and lower-case extension
+# (We assume we have bash 4+ if we have ifuse)
+for File in *; do Date=$(date -r "$File" +%Y-%m-%d_%H-%M-%S 2>/dev/null)
+Ext="${File##*.}"; Ext="${Ext,,}"
+Dest="$ThisBatch/$Date.$Ext"; counter=1; while [ -e "$Dest" ]; do ((counter++)); Dest="$ThisBatch/$Date_$(printf '%03d' $counter).$Ext"; done
+mv -v "$File" "$Dest"; done; cd ..; done
+chown -R "$User:$User" "$ThisBatch"
 umount /mnt
